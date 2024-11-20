891fc15ff087e9e67a58ef862bc1938b56f1c89b
Simon Duranowski <sduranowski1@gmail.com>
2024-11-20 16:23:21 +0100

    feat: modifying edit page

---
+++ frontend/src/Resources/EnovaProduct/EnovaProductById.js
@@ -0,0 +1,249 @@
+import React, {useState, useEffect, useCallback} from 'react';
+import {useParams, useNavigate, Link} from 'react-router-dom';
+import {
+    Box,
+    TextField,
+    Select,
+    MenuItem,
+    InputLabel,
+    FormControl,
+    Button,
+    CircularProgress,
+    Typography, IconButton
+} from '@mui/material';
+import { CloudUpload as CloudUploadIcon } from '@mui/icons-material';
+import {fetchToken} from "../../utils/fetchToken";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import CustomTableHead from "../../Components/AdminTableHead/CustomTableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import CustomCheckbox from "../../Components/AdminCheckbox/CustomCheckbox";
+import TableBody from "@mui/material/TableBody";
+import TableContainer from "@mui/material/TableContainer";
+
+const EnovaProductEdit = () => {
+    const { id } = useParams();  // Get the id from the route parameter
+    const [token, setToken] = useState(null);
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // Fetch the token first
+                const token = await fetchToken();
+                setToken(token);
+
+                // Use the token to fetch the actual data
+                const response = await fetch('https://se-europe-test.pl/api/PanelWWW_API/DajTowarWgId', {
+                    method: 'POST',
+                    headers: {
+                        'accept': 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        parametr: id,
+                    }),
+                });
+
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch product data');
+                }
+
+                const result = await response.json();
+                setData(result);
+                setLoading(false); // Set loading to false after data is fetched
+            } catch (error) {
+                setError(error.message);
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [id]); // Empty dependency array to run only once on mount
+
+    const renderDetails = (data) => {
+        const details = {
+            '% wzrostu': data.procWzrostu,
+            'Capacity': data.capacity,
+            'Części zamienne': data.replacementParts ? 'True' : 'False',
+            'DDP PL': data.ddpPl ? 'True' : 'False',
+            'Producent': data.producent,
+            // Add more fields as required
+        };
+
+        return Object.entries(details).map(([key, value]) => (
+            <TableRow key={key}>
+                <TableCell sx={{ fontWeight: 'bold' }}>{key}</TableCell>
+                <TableCell>{value || 'N/A'}</TableCell>
+            </TableRow>
+        ));
+    };
+
+    const renderPrices = (product) => {
+        // Define the desired price names
+        const desiredPrices = [
+            'OEM PLN',
+            'OEM EUR',
+            'PRO PLN',
+            'PRO EUR',
+            'HAN EUR',
+            'HAN PLN',
+            'Dealer Detal',
+            'Volvo PL',
+            'Volvo EUR',
+            'End User',
+        ];
+
+        // Map through desiredPrices and fetch matching price objects
+        const prices = desiredPrices.map((priceName) => {
+            const matchedPrice = product.listaCen.find((price) => price.nazwa === priceName);
+            return {
+                nazwa: priceName,
+                netto: matchedPrice ? matchedPrice.netto : null,
+                waluta: matchedPrice ? matchedPrice.waluta : 'N/A',
+            };
+        });
+
+        // Render table rows dynamically
+        return prices.map((price, index) => (
+            <TableRow key={index}>
+                <TableCell>{price.waluta}</TableCell>
+                <TableCell>{price.netto ? `${price.netto}` : 'N/A'}</TableCell>
+                <TableCell>{price.nazwa}</TableCell>
+            </TableRow>
+        ));
+    };
+
+
+    if (!data) {
+        return (
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                height="100vh" // Or use specific height if you want it in a smaller area
+                width="100%"
+            >
+                <CircularProgress />
+            </Box>
+        );
+    };
+
+    return (
+        <div>
+            {loading && (
+                <Box
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    height="100%"
+                    position="absolute"
+                    top={0}
+                    left={0}
+                    right={0}
+                    bottom={0}
+                    bgcolor="rgba(255, 255, 255, 0.7)"
+                >
+                    <CircularProgress />
+                </Box>
+            )}
+
+            {!loading && error && (
+                <Box sx={{ color: 'red', textAlign: 'center', padding: 2 }}>
+                    {error}
+                </Box>
+            )}
+
+            {!loading && data ? (
+                <div>
+                    <TableContainer component={Paper} sx={{position: 'relative', marginTop: "15px"}}>
+
+                        <Table>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell sx={{fontWeight: 'bold'}}>Id</TableCell>
+                                    <TableCell>{data.id || 'N/A'}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell sx={{fontWeight: 'bold'}}>Product Name</TableCell>
+                                    <TableCell>{data.nazwa || 'N/A'}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell sx={{fontWeight: 'bold'}}>Code</TableCell>
+                                    <TableCell>{data.kod || 'N/A'}</TableCell>
+                                </TableRow>
+                                {/*<TableRow>*/}
+                                {/*    <TableCell sx={{ fontWeight: 'bold' }}>% Increase</TableCell>*/}
+                                {/*    <TableCell>{data.procWzrostu || 'N/A'}</TableCell>*/}
+                                {/*</TableRow>*/}
+                                {/*<TableRow>*/}
+                                {/*    <TableCell sx={{ fontWeight: 'bold' }}>Replacement Parts</TableCell>*/}
+                                {/*    <TableCell>{data.replacementParts || 'N/A'}</TableCell>*/}
+                                {/*</TableRow>*/}
+                                <TableRow>
+                                    <TableCell sx={{fontWeight: 'bold'}}>Unit</TableCell>
+                                    <TableCell>{data.jednostka || 'N/A'}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell sx={{fontWeight: 'bold'}}>Quantity</TableCell>
+                                    <TableCell>{data.Ilosc || 'N/A'}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell sx={{fontWeight: 'bold'}}>Availability</TableCell>
+                                    <TableCell>{data.stanMagazynowy || 'N/A'}</TableCell>
+                                </TableRow>
+                                {/*<TableRow>*/}
+                                {/*    <TableCell sx={{ fontWeight: 'bold' }}>Price (Netto)</TableCell>*/}
+                                {/*    <TableCell>{data.netto || 'N/A'}</TableCell>*/}
+                                {/*</TableRow>*/}
+                            </TableBody>
+                        </Table>
+
+
+                    </TableContainer>
+
+                    <Typography variant="h5" component="h5" sx={{marginTop: "20px"}}>Attributes</Typography>
+                    <TableContainer component={Paper} sx={{position: 'relative', marginTop: "15px"}}>
+                        <Table>
+                            {/*<CustomTableHead>*/}
+                            {/*    <TableRow>*/}
+                            {/*        <TableCell>Name</TableCell>*/}
+                            {/*        <TableCell>Value</TableCell>*/}
+                            {/*    </TableRow>*/}
+                            {/*</CustomTableHead>*/}
+                            <TableBody>
+                                {/* Render Details Section */}
+                                {renderDetails(data)}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+
+                    <Typography variant="h5" component="h5" sx={{marginTop: "20px"}}>Prices</Typography>
+                    <TableContainer component={Paper} sx={{position: 'relative', marginTop: "15px"}}>
+                        <Table>
+                            <CustomTableHead>
+                                <TableRow>
+                                    <TableCell>Currency</TableCell>
+                                    <TableCell>Netto</TableCell>
+                                    <TableCell>Name</TableCell>
+                                </TableRow>
+                            </CustomTableHead>
+                            <TableBody>
+                                {/* Render Details Section */}
+                                {renderPrices(data)}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
+            ) : (
+                <p>Loading data...</p>
+    )}
+</div>
+    );
+};
+
+export default EnovaProductEdit;
--- frontend/src/Resources/EnovaProduct/EnovaProductEdit.js
+++ frontend/src/Resources/EnovaProduct/EnovaProductEdit.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import React, {useState, useEffect, useCallback} from 'react';
+import {useParams, useNavigate, Link} from 'react-router-dom';
 import {
     Box,
     TextField,
@@ -12,17 +12,33 @@ import {
     Typography, IconButton
 } from '@mui/material';
 import { CloudUpload as CloudUploadIcon } from '@mui/icons-material';
+import {fetchToken} from "../../utils/fetchToken";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import CustomTableHead from "../../Components/AdminTableHead/CustomTableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import CustomCheckbox from "../../Components/AdminCheckbox/CustomCheckbox";
+import TableBody from "@mui/material/TableBody";
+import TableContainer from "@mui/material/TableContainer";
+import EnovaProductById from "./EnovaProductById";
 
 const EnovaProductEdit = () => {
-    const { id } = useParams(); // Get the product ID from the URL
+    const {id} = useParams(); // Get the product ID from the URL
     const navigate = useNavigate();
     const [product, setProduct] = useState(null);
+    const [productId, setProductId] = useState(null);
     const [brands, setBrands] = useState([]);
     const [variants, setVariants] = useState([]);
     const [categories, setCategories] = useState([]);
     const [loading, setLoading] = useState(true);
     const [image, setImage] = useState(null);
     const [imagePreview, setImagePreview] = useState(null);
+    const [token, setToken] = useState(null);
+    const [products, setProducts] = useState([]);
+    const [totalItems, setTotalItems] = useState(0);
+    const [error, setError] = useState(null);
+
 
     useEffect(() => {
         const fetchProductData = async () => {
@@ -56,7 +72,7 @@ const EnovaProductEdit = () => {
     }, [id]);
 
     const handleChange = (event) => {
-        setProduct({ ...product, [event.target.name]: event.target.value });
+        setProduct({...product, [event.target.name]: event.target.value});
     };
 
     const handleFileChange = (event) => {
@@ -130,9 +146,6 @@ const EnovaProductEdit = () => {
         }
     };
 
-
-
-
     if (loading) {
         return (
             <Box
@@ -142,106 +155,130 @@ const EnovaProductEdit = () => {
                 height="100vh" // Or use specific height if you want it in a smaller area
                 width="100%"
             >
-                <CircularProgress />
+                <CircularProgress/>
             </Box>
         );
     }
 
     return (
-        <Box component="form" sx={{ maxWidth: 600, margin: 'auto' }}>
-            <FormControl fullWidth margin="normal">
-                <InputLabel id="brand-label">Brand</InputLabel>
-                <Select
-                    labelId="brand-label"
-                    name="braid"
-                    value={product?.braid || ''}
-                    onChange={handleChange}
-                    label="Brand"
-                >
-                    {brands.map((brand) => (
-                        <MenuItem key={brand.id} value={brand.id}>
-                            {brand.name}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
+        <div>
+            <Box display="flex" justifyContent="space-between" sx={{padding: 2}}>
+                {/* Form Container */}
+                <Box sx={{
+                    flex: 1,
+                    marginRight: 2,
+                    height: 'calc(100vh - 96px)',
+                    overflow: 'auto', // Allows scrolling
+                    '::-webkit-scrollbar': {
+                        display: 'none', // Hides scrollbar in WebKit browsers (Chrome, Safari)
+                    },
+                    scrollbarWidth: 'none', // Hides scrollbar in Firefox
+                }}>
+                    <EnovaProductById /> {/* Assuming this component renders your table */}
+                </Box>
 
-            <FormControl fullWidth margin="normal">
-                <InputLabel id="variant-label">Variant</InputLabel>
-                <Select
-                    labelId="variant-label"
-                    name="varid"
-                    value={product?.varid || ''}
-                    onChange={handleChange}
-                    label="Variant"
-                >
-                    {variants.map((variant) => (
-                        <MenuItem key={variant.id} value={variant.id}>
-                            {variant.variantname}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
+                {/* Sticky Section */}
+                <Box sx={{flex: 1, maxWidth: 600, position: 'sticky', top: 16}}>
+                    <Box component="form" sx={{maxWidth: '100%'}}>
+                        <FormControl fullWidth margin="normal">
+                            <InputLabel id="brand-label">Brand</InputLabel>
+                            <Select
+                                labelId="brand-label"
+                                name="braid"
+                                value={product?.braid || ''}
+                                onChange={handleChange}
+                                label="Brand"
+                            >
+                                {brands.map((brand) => (
+                                    <MenuItem key={brand.id} value={brand.id}>
+                                        {brand.name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
 
-            <FormControl fullWidth margin="normal">
-                <InputLabel id="category-label">Category</InputLabel>
-                <Select
-                    labelId="category-label"
-                    name="catid"
-                    value={product?.catid || ''}
-                    onChange={handleChange}
-                    label="Category"
-                >
-                    {categories.map((category) => (
-                        <MenuItem key={category.id} value={category.id}>
-                            {category.name}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
+                        <FormControl fullWidth margin="normal">
+                            <InputLabel id="variant-label">Variant</InputLabel>
+                            <Select
+                                labelId="variant-label"
+                                name="varid"
+                                value={product?.varid || ''}
+                                onChange={handleChange}
+                                label="Variant"
+                            >
+                                {variants.map((variant) => (
+                                    <MenuItem key={variant.id} value={variant.id}>
+                                        {variant.variantname}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
 
-            {/* Image Upload */}
-            <Box marginTop={2}>
-                <input
-                    accept="image/*"
-                    id="upload-image"
-                    type="file"
-                    onChange={handleFileChange}
-                    style={{ display: 'none' }}
-                />
-                <label htmlFor="upload-image">
-                    <IconButton
-                        color="primary"
-                        component="span"
-                        sx={{ width: 40, height: 40, marginRight: 1 }}
-                    >
-                        <CloudUploadIcon />
-                    </IconButton>
-                    {imagePreview ? (
-                        <img src={imagePreview} alt="Preview" style={{ maxWidth: '100%', maxHeight: '200px' }} />
-                    ) : product?.imagePath ? (
-                        // Display existing image if available
-                        <img
-                            src={`https://se-europe-test.pl${product.imagePath}`}
-                            alt="Existing product image"
-                            style={{ maxWidth: '100%', maxHeight: '200px' }}
-                        />
-                    ) : (
-                        <span>Upload Image</span>
-                    )}
-                </label>
+                        <FormControl fullWidth margin="normal">
+                            <InputLabel id="category-label">Category</InputLabel>
+                            <Select
+                                labelId="category-label"
+                                name="catid"
+                                value={product?.catid || ''}
+                                onChange={handleChange}
+                                label="Category"
+                            >
+                                {categories.map((category) => (
+                                    <MenuItem key={category.id} value={category.id}>
+                                        {category.name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+
+                        {/* Image Upload */}
+                        <Box marginTop={2}>
+                            <input
+                                accept="image/*"
+                                id="upload-image"
+                                type="file"
+                                onChange={handleFileChange}
+                                style={{display: 'none'}}
+                            />
+                            <label htmlFor="upload-image">
+                                <IconButton
+                                    color="primary"
+                                    component="span"
+                                    sx={{width: 40, height: 40, marginRight: 1}}
+                                >
+                                    <CloudUploadIcon/>
+                                </IconButton>
+                                {imagePreview ? (
+                                    <img
+                                        src={imagePreview}
+                                        alt="Preview"
+                                        style={{maxWidth: '100%', maxHeight: '200px'}}
+                                    />
+                                ) : product?.imagePath ? (
+                                    <img
+                                        src={`https://se-europe-test.pl${product.imagePath}`}
+                                        alt="Existing product image"
+                                        style={{maxWidth: '100%', maxHeight: '200px', borderRadius: '15px'}}
+                                    />
+                                ) : (
+                                    <span>Upload Image</span>
+                                )}
+                            </label>
+                        </Box>
+
+                        <Box display="flex" justifyContent="space-between" marginTop={2}>
+                            <Button variant="outlined" onClick={() => navigate('/admin/enova-products')}>
+                                Cancel
+                            </Button>
+                            <Button variant="contained" color="primary" onClick={handleSave}>
+                                Save
+                            </Button>
+                        </Box>
+                    </Box>
+                </Box>
+
             </Box>
-
-            <Box display="flex" justifyContent="space-between" marginTop={2}>
-                <Button variant="outlined" onClick={() => navigate('/admin/enova-products')}>
-                    Cancel
-                </Button>
-                <Button variant="contained" color="primary" onClick={handleSave}>
-                    Save
-                </Button>
-            </Box>
-        </Box>
+        </div>
     );
 };
-
 export default EnovaProductEdit;

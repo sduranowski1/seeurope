012d93417b890e908f370a9fa23f5ba13cc329ce
Simon Duranowski <sduranowski1@gmail.com>
2025-03-25 13:11:52 +0100

    refactor: different id name

--- src/Entity/Enova/UserEnova.php
+++ src/Entity/Enova/UserEnova.php
@@ -96,7 +96,7 @@ class UserEnova implements UserInterface, PasswordAuthenticatedUserInterface
 //    #[Groups(['userEnova:read', 'userEnova:create', 'userEnova:update'])]
 //    private ?EnovaPerson $enovaPerson = null;
     #[ORM\ManyToOne(targetEntity: TestEnovaContactPerson::class)]
-    #[ORM\JoinColumn(name: 'enova_person_id', referencedColumnName: 'id', nullable: true)]
+    #[ORM\JoinColumn(name: 'enova_person_id', referencedColumnName: 'uuid', nullable: true)]
     #[Groups(['userEnova:read', 'userEnova:create', 'userEnova:update'])]
     private ?TestEnovaContactPerson $enovaPerson = null;
 
